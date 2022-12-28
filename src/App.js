627d532ffb2c80a75d655ca4a86c3e065dc1f19f
danielferra <dferraioli@hotmail.com>
2022-12-28 12:39:13 -0300

    agregue archivos de firebase

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useThemeHook } from "./GlobalComponents/ThemeProvider";
 import Header from './components/Header'
 import { Router } from "@reach/router";
-
 import Home from './Pages/Home'
 import Cart from './Pages/Cart'
+import { collection, getDocs} from 'firebase/firestore'
+import { useEffect } from "react";
+import db from './firebase/firebaseConfig'
+
 
 function App() {
+  useEffect(() => {
+    const obtenerDatos = async() =>{
+      const datos = await getDocs(collection(db,'usuarios'))
+      console.log(datos);
+    }
+    obtenerDatos();
+   }, []);
+
+
   const [theme] = useThemeHook();
   return (
     <main className={theme ? 'bg-black' : 'bg-light-2'} style={{height: '100vh', overflowY: 'auto'}}>
